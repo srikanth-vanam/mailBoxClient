9f546a5fe2538f4939d4a43eaa33a6e8cdcf6d20
srikanth-vanam <B172320-@rgukt.ac.in>
2023-11-30 13:39:40 +0530

    sign Page + first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignUP from "./components/SignUp/SignUP";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Hi there!</h1> */}
+      <SignUP />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SignUp/SignUP.js
@@ -0,0 +1,107 @@
+import { useRef, useState } from "react";
+import { Card, Form, FormControl, FormLabel, Button } from "react-bootstrap";
+
+const SignUP = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  //Inputs
+  const emailInputRef=useRef();
+  const passwordInputRef=useRef();
+  // 
+  const loginHandler=()=>{
+    setIsLogin((prevState)=>{
+      return !prevState;
+    })
+  }
+  // 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const userCredentials = {
+      email: emailInputRef.current.value,
+      password: passwordInputRef.current.value,
+    };
+    postUserData(userCredentials);
+  };
+
+  const postUserData = (obj) => {
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC6fdqT-BKSYvdgNjdso0biEIf45XQLPXk";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC6fdqT-BKSYvdgNjdso0biEIf45XQLPXk";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: obj.email,
+        password: obj.password,
+        returnSecureToken: true,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("error in signUP? Already a user try login..");
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        // dispatch(Authactions.setToken(data.idToken));
+        // splitting email and removing '.com' from it
+        const email = obj.email.split(".com")[0];
+        // dispatch(Authactions.setEmailId(email));
+        console.log("added token,email to store");
+        localStorage.setItem("token", data.idToken);
+        localStorage.setItem("email", email);
+        console.log("added token to localStorage");
+        console.log(data.idToken);
+        console.log("user registered successfully");
+        // history.replace("/home");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  return (
+    <>
+      <Card className="w-25 mt-5 m-auto p-2">
+        <h2 className="m-auto">{isLogin ? "Login" : "SignUP"}</h2>
+        <Form onSubmit={submitHandler}>
+          <FormLabel htmlFor="email">
+            Email
+          </FormLabel>
+          <FormControl type="email" className="w-90%" placeholder="Enter Email" required ref={emailInputRef}></FormControl>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <FormControl type="password"placeholder="Enter Password" ref={passwordInputRef} required></FormControl>
+          {!isLogin ? (
+            <>
+              <FormLabel htmlFor="c-pswd">Confirm Password</FormLabel>
+              <FormControl required placeholder="Re-type Password" ></FormControl>
+            </>
+          ) : (
+            ""
+          )}
+          <Button className=" m-auto mt-2 d-block" type="submit" >
+            {isLogin ? "Login" : "Sign Up"}
+          </Button>
+        </Form>
+        <Button
+          className=" d-block m-auto mt-2"
+          type="button"
+          variant="secondary"
+          onClick={loginHandler}
+        >
+          <p>
+            {isLogin
+              ? "Don't have an account? SignUP"
+              : "Already have an account?Login"}
+          </p>
+        </Button>
+      </Card>
+    </>
+  );
+};
+
+export default SignUP;
